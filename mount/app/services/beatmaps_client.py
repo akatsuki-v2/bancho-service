a5622a97227d40705a0314e86cf4ae814efbe6bb
cmyui <cmyuiosu@gmail.com>
2022-10-17 00:46:22 -0400

    feat: beatmap sets

--- mount/app/services/beatmaps_client.py
+++ mount/app/services/beatmaps_client.py
@@ -2,6 +2,7 @@ from app.common import logging
 from app.common import settings
 from app.common.context import Context
 from app.models.beatmaps import Beatmap
+from app.models.beatmapsets import Beatmapset
 
 SERVICE_URL = "http://beatmaps-service"
 
@@ -52,3 +53,51 @@ class BeatmapsClient:
             return None
 
         return [Beatmap(**rec) for rec in response.json['data']]
+
+    # beatmapsets
+
+    async def get_beatmapset(self, set_id: int) -> Beatmapset | None:
+        response = await self.ctx.http_client.service_call(
+            method="GET",
+            url=f"{SERVICE_URL}/v1/beatmapsets/{set_id}",
+        )
+        if response.status_code not in range(200, 300):
+            logging.error("Failed to get beatmapset",
+                          status=response.status_code,
+                          response=response.json)
+            return None
+
+        return Beatmapset(**response.json['data'])
+
+    async def get_beatmapsets(self, set_id: int | None = None,
+                              artist: str | None = None,
+                              creator: str | None = None,
+                              title: str | None = None,
+                              nsfw: bool | None = None,
+                              ranked_status: int | None = None,
+                              status: str | None = None,
+                              page: int = 1,
+                              page_size: int = settings.DEFAULT_PAGE_SIZE,
+                              ) -> list[Beatmapset] | None:
+        response = await self.ctx.http_client.service_call(
+            method="GET",
+            url=f"{SERVICE_URL}/v1/beatmapsets",
+            params={
+                "set_id": set_id,
+                "artist": artist,
+                "creator": creator,
+                "title": title,
+                "nsfw": nsfw,
+                "ranked_status": ranked_status,
+                "status":  status,
+                "page": page,
+                "page_size": page_size,
+            },
+        )
+        if response.status_code not in range(200, 300):
+            logging.error("Failed to get beatmapsets",
+                          status=response.status_code,
+                          response=response.json)
+            return None
+
+        return [Beatmapset(**rec) for rec in response.json['data']]
