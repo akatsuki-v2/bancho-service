33916fe48d3491cf30027b864e7a0bf2d96cd685
cmyui <cmyuiosu@gmail.com>
2022-10-22 22:57:56 -0400

    feat: more configurable/advanced logging set up

--- mount/app/api_boot.py
+++ mount/app/api_boot.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from app.api.rest import init_api
 from app.common import logging
 from app.common import settings
-logging.init_logging(log_level=settings.LOG_LEVEL)
+
+logging.configure_logging(app_env=settings.APP_ENV,
+                          log_level=settings.LOG_LEVEL)
 
 api = init_api()
--- mount/app/common/logging.py
+++ mount/app/common/logging.py
@@ -1,32 +1,85 @@
 from __future__ import annotations
 
-import logging
+import logging as stdlib_logging
+import os
+from contextvars import ContextVar
 
 import structlog
+from structlog.types import EventDict
+from structlog.types import WrappedLogger
 
-logger = structlog.get_logger()
+_ROOT_LOGGER = stdlib_logging.getLogger("service-root")
+
+_REQUEST_ID_CONTEXT = ContextVar("request_id")
 
 
-def init_logging(log_level: str | int) -> None:
-    logging.basicConfig(level=log_level)
-    logger.info("Logging initialized")
+def get_logger(name: str | None = None) -> structlog.BoundLogger:
+    return structlog.wrap_logger(_ROOT_LOGGER, logger_name=name or "root")
+
+
+def log_as_text(app_env: str) -> bool:
+    return app_env == "local"
+
+
+def add_process_id(_: WrappedLogger, __: str, event_dict: EventDict) -> EventDict:
+    event_dict["process_id"] = os.getpid()
+    return event_dict
+
+
+def add_request_id(_: WrappedLogger, __: str, event_dict: EventDict) -> EventDict:
+    if request_id := _REQUEST_ID_CONTEXT.get(None):
+        event_dict["request_id"] = request_id
+
+    return event_dict
+
+
+def configure_logging(app_env: str, log_level: str | int) -> None:
+    if log_as_text(app_env):
+        renderer = structlog.dev.ConsoleRenderer(colors=True)
+    else:
+        renderer = structlog.processors.JSONRenderer()
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processor=renderer,
+        foreign_pre_chain=[
+            structlog.processors.TimeStamper(fmt="iso", key="timestamp"),
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            add_process_id,
+            add_request_id,
+        ],
+    )
+
+    handler = stdlib_logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel((log_level))
+
+    # brute force control of the message format & level
+    existing_loggers = [
+        stdlib_logging.getLogger(name)
+        for name in stdlib_logging.root.manager.loggerDict
+    ]
+    for logger in existing_loggers:
+        logger.handlers = []
+        logger.addHandler(handler)
+        logger.setLevel(log_level)
 
 
 def debug(*args, **kwargs) -> None:
-    return logger.debug(*args, **kwargs)
+    return get_logger().debug(*args, **kwargs)
 
 
 def info(*args, **kwargs) -> None:
-    return logger.info(*args, **kwargs)
+    return get_logger().info(*args, **kwargs)
 
 
 def warning(*args, **kwargs) -> None:
-    return logger.warning(*args, **kwargs)
+    return get_logger().warning(*args, **kwargs)
 
 
 def error(*args, **kwargs) -> None:
-    return logger.error(*args, **kwargs)
+    return get_logger().error(*args, **kwargs)
 
 
 def critical(*args, **kwargs) -> None:
-    return logger.critical(*args, **kwargs)
+    return get_logger().critical(*args, **kwargs)
